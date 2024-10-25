5b1513e5483d7187c8e5c677afd4468bf35fead9
Paguiel <119151331+paguielng@users.noreply.github.com>
2024-10-25 18:53:59 +0200

    Delete Compteur-sequenceur.py

--- Compteur-sequenceur.py
+++
@@ -1,9 +0,0 @@
-from datetime import time
-def Compteur(n) -> int:
-    compteur = 0
-    for i in range(n):
-        compteur += 1
-    return compteur
-
-Détéction de la vibration : Détéction du décollage
-    def decolfuse(
